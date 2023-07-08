6a720eeccdc79ceba453d703c7fb1a6cecd8aac6
Bakhita87 <bakhitaotieno@gmail.com>
2023-07-09 01:48:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word === word.split("").reverse().join("");
 }
 
 /* 
   Add your pseudocode here
+  create a function that has a parameter word
+  and return true if word is a palindrome, false otherwise
 */
 
 /*
-  Add written explanation of your solution here
+  Add a written explanation of your solution here
+  to the problem statement
+  and for the tests
+  You can then run `node index.js` to view these console logs
+  to help you debug your solution
+  You can also run `node index.js` to view these console logs
+  to help you debug your solution
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+   
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
